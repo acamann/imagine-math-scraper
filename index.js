69a662de9cfa4f80f8a056997ee353a91f5a0ceb
Camann <Andrew.Camann@springbranchisd.com>
2020-02-12 16:03:00 -0600

    include parameter for which subset to crawl

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // run this file using login credentials for Imagine Math:
 //  > node index.js [username] [password]
 
+const startWithStudentNumber = 283;
+const endWithStudentNumber = 1000;
+
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 let students = [];
@@ -12,7 +15,7 @@ function delay(timeout) {
 }
 
 class StudentInfo {
-    constructor (first, last, grade, period, studentProgressLink, fullName = "", mostRecentCertificateUrl = "", studentAvatarSVGUrl = "") {
+    constructor (first, last, grade, period, studentProgressLink, fullName = "", mostRecentCertificateUrl = "", studentAvatarSVGUrl = "", dateCrawled = "") {
         this.first = first;
         this.last = last;
         this.grade = grade;
@@ -21,6 +24,7 @@ class StudentInfo {
         this.fullName = fullName;
         this.mostRecentCertificateUrl = mostRecentCertificateUrl;
         this.studentAvatarSVGUrl = studentAvatarSVGUrl;
+        this.dateCrawled = dateCrawled;
     }
 }
 
@@ -50,6 +54,10 @@ async function asyncForEach(array, callback) {
 }
 
 var scrapeStudentProfiles = async () => {
+    const username = process.argv[2];
+    const password = process.argv[3];
+    if (username == "" || password == "") throw new Error("Must pass Imagine Math username & password as arguments:> node index.js [username] [password]");
+
     const browser = await puppeteer.launch({ });
     const page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 800});
@@ -58,15 +66,19 @@ var scrapeStudentProfiles = async () => {
     await page.goto('https://math.imaginelearning.com/');
     
     // log in
-    await page.type('#student_username', process.argv[2]);
-    await page.type('#student_password', process.argv[3]);
+    await page.type('#student_username', username);
+    await page.type('#student_password', password);
     await page.click('#btn_student_sign_in');
     await page.waitForNavigation();
 
     // fetch json data of all student profile pages:
     let rawStudentData = fs.readFileSync("./student-profile-data/student-profile-links.json");
     let studentData = JSON.parse(rawStudentData);
-    await asyncForEach(studentData, async (item) => {
+    await asyncForEach(studentData, async (item, index) => {
+        
+        // control the subset of data to crawl
+        if (index < (startWithStudentNumber - 1) || index > (endWithStudentNumber - 1)) return;
+
         let student = new StudentInfo(item.First, item.Last, item.Grade, item["Math Period"], item["Student Progress Link"]);
         if(student.studentProgressLink.length == 0) return;
 
@@ -74,7 +86,8 @@ var scrapeStudentProfiles = async () => {
 
             // go to each student profile page
             await page.goto(student.studentProgressLink);
-            await page.waitForSelector('.lessonActivity--certificate > a', { timeout: 3000 });
+            await page.waitForSelector('.lessonActivity--certificate > a', { timeout: 20000 });
+            
             
             // select all the .lessonActivity--certificate spans that have an anchor tag (the lessons with certificates)
             let certificateLinks = await page.evaluate(() => {
@@ -85,7 +98,7 @@ var scrapeStudentProfiles = async () => {
 
             // access certificate page with avatar
             await page.goto(student.mostRecentCertificateUrl);
-            await page.waitForSelector('.name', { timeout: 3000 });
+            await page.waitForSelector('.name', { timeout: 10000 });
             student.fullName = await page.evaluate(() => {
                 return document.querySelector('.name').innerHTML;
             });
@@ -105,14 +118,17 @@ var scrapeStudentProfiles = async () => {
                     console.error(err);
                     return;
                 }
-                console.log(`SVG successfully saved for ${student.fullName}`);
+                console.log(`SVG successfully saved for ${student.fullName} (index: ${index})`);
             });
 
             
 
-        } catch (error) {
+        } catch (error) {            
+            await page.screenshot({path: `test-screenshots/Error-${student.grade}-${student.last}-${student.first}.png`});
             console.error(`Error crawling student profile for ${student.first} ${student.last}: ` + error);
         }
+
+        student.dateCrawled = Date(Date.now()).toString();
 
         // save all the links we crawled to a CSV row to be added to a CSV file once we are all done
         students.push(student);
